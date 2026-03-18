e0e6d87f0eb00bde61e99763bc8cce49fd93c5b8
Noémie <mallet.noemie46@gmail.com>
2026-03-18 16:59:05 +0100

    changement de chemin pour les styles

--- api/header-logged.php
+++ api/header-logged.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= htmlspecialchars($title, ENT_QUOTES, 'UTF-8') ?> Imgshare </title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="api/styles.css">
 </head>
 
 <body>
