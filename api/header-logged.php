2bada5b7ba1d1647f587c65972747156c198d2ff
Noémie <mallet.noemie46@gmail.com>
2026-03-24 17:24:01 +0100

    ajustements du css afin que tout s'affiche

--- api/header-logged.php
+++ api/header-logged.php
@@ -5,9 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= htmlspecialchars($title, ENT_QUOTES, 'UTF-8') ?> Imgshare </title>
-    <!-- Polices auto-hébergées (place les fichiers de police dans api/fonts/) -->
-    <link rel="stylesheet" href="fonts.css">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="./api/styles.css">
 </head>
 
 <body>
